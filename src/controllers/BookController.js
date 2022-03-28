cf7f0c285629955b4dbcffc89983c76aaeac0804
JyotiPratap <jyotiprataptiwarijee1@gmail.com>
2022-03-28 11:26:24 +0530

    jyoti pratap tiwari

--- src/controllers/BookController.js
+++ src/controllers/BookController.js
@@ -1,5 +1,6 @@
 const bookmodel = require("../models/BookModel.js");
 const objectId = require('mongoose').Types.ObjectId
+const reviewmodel = require("../models/ReviewModel.js")
 
 const isValid = function (value) {
     if (typeof value == 'undefined' || value === null) return false
@@ -79,8 +80,133 @@ const booklist = async function (req, res) {
         return res.status(500).send({ status: false, msg: err.message })
 
     }
+};
+
+const getBookReview = async function (req, res) {
+    try {
+        let bookId = req.params.bookId
+
+        if (!isValid(bookId)) {
+            return res.status(400).send({ status: false, msg: "bookId required" })
+
+        }
+        let reviewList = await bookmodel.findOne({ bookId: bookId })
+        if (!reviewList) {
+            return res.status(404).send({ status: false, msg: "not found " })
+        }
+        if (!objectId.isValid(bookId)) {
+            return res.status(400).send({ status: false, msg: "bookId invalid" })
+        }
+        let result = {
+            _id: reviewList._id,
+            title: reviewList.title,
+            excerpt: reviewList.excerpt,
+            userId: reviewList.userId,
+            category: reviewList.category,
+            subcategory: reviewList.subcategory,
+            deleted: reviewList.isDeleted,
+            reviews: reviewList.reviews,
+            deletedAt: reviewList.deletedAt,
+            releasedAt: reviewList.releasedAt,
+            createdAt: reviewList.createdAt,
+            updatedAt: reviewList.updatedAt
+        }
+        let eachReview = await reviewmodel.find({ bookId: bookId }).select({ bookId: 1, reviewedBy: 1, reviewedAt: 1, rating: 1, review: 1 })
+        if (!eachReview) {
+            result['reviewsData'] = "No review for this books"
+            return res.status(200).send({ status: false, data: result })
+        }
+        result['reviewsData'] = eachReview
+        return res.status(200).send({ status: false, data: result })
+
+
+
+    }
+    catch (err) {
+        return res.status(500).send({ status: false, msg: err.message })
+    }
+
 
 }
+
+
+const updatebook = async function (req, res) {
+    try {
+        let bookId = req.params.bookId
+        let { title, excerpt, ISBN } = req.body
+        if (!(title && excerpt && ISBN)) {
+            return res.status(400).send({ status: false, msg: "required data should be title ,excerpt,date ,ISBN" })
+        }
+        let dupBook = await bookmodel.findOne({ title: title, ISBN: ISBN })
+        if (dupBook) {
+            return res.status(400).send({ status: false, msg: "this title and ISBN already updated" })
+        }
+        if (!bookId) {
+            return res.status(400).send({ status: false, msg: "bookId is required" })
+        }
+        if (!objectId.isValid(bookId)) {
+            return res.status(400).send({ status: false, msg: "invalid bookId" })
+        }
+        let date = moment()
+        let updateBookData = { title: title, excerpt: excerpt, releasedAt: date, ISBN: ISBN }
+        let updated = await bookmodel.findOneAndUpdate({ _id: bookId, isDeleted: false }, { $set: updateBookData }, { new: true })
+        if (!updated) {
+            return res.status(404).send({ status: false, msg: "data not found " })
+        }
+        return res.status(200).send({ status: false, data: updated })
+    }
+    catch (err) {
+        return res.status(500).send({ status: false, msg: err.message })
+    }
+};
+
+const deleteBook = async function(req,res){
+    try{
+        let bookId = req.params.bookId
+    if (!bookId) {
+        return res.status(400).send({ status: false, msg: "required bookId" })
+    }
+    if (!objectId.isValid(bookId)) {
+        return res.status(400).send({ status: false, msg: "invalid bookId" })
+    }
+    let bookIdNotExist = await bookmodel.findOne({_id:bookId})
+    if (!bookIdNotExist) {
+        return res.status(404).send({ status: false, msg: "not found" })
+    }
+    if(bookIdNotExist.isDeleted == true){
+        return res.status(400).send({ status: false, msg: "This bookId already deleted" })
+
+    }
+
+    let data = {isDeleted:true,deletedAt:moment()}
+    const deleteData = await bookmodel.findOneAndUpdate({_id:bookId,isDeleted:false},{$set:data},{new:true})
+    let result ={
+        _id:deleteData._id,
+        title:deleteData.title,
+        excerpt:deleteData.excerpt,
+        userId: deleteData.userId,
+        category: deleteData.category,
+        subcategory: deleteData.subcategory,
+        deleted: deleteData.isDeleted,
+        reviews: deleteData.reviews,
+        deletedAt: deleteData.deletedAt,
+        releasedAt: deleteData.releasedAt,
+        createdAt: deleteData.createdAt,
+        updatedAt: deleteData.updatedAt
+
+    }
+    return res.status(200).send({status:false,data:result})
+    }
+    catch(err){
+        return res.status(500).send({status:false,msg:err.message})
+    }
+}
+
+
+
+
 module.exports.createbook = createbook
- module.exports.booklist=booklist
-// module.exports.getbook=getbook
+module.exports.booklist=booklist
+module.exports.getBookReview=getBookReview
+module.exports.updatebook=updatebook
+module.exports.deleteBook=deleteBook
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,7 +6,12 @@ const ProjectMiddleware= require("../Middleware/middleware")
 
 router.post("/register",usercontroller.user)
 router.post("/login",usercontroller.loginUser)
+
 router.post("/createbook",bookcontroller.createbook)
 router.get("/getbook",bookcontroller.booklist)
-// router.get("books/:bookId",ProjectController.getbook)
+router.get("/books/:bookId",bookcontroller.getBookReview)
+router.put("/books/:bookId",bookcontroller.updatebook)
+router.delete("/books/:bookId",bookcontroller.deleteBook)
+
+
 module.exports = router;
