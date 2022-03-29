1ef0b5780f34e552e18be70cad24ce6e59a4a8bd
JyotiPratap <jyotiprataptiwarijee1@gmail.com>
2022-03-29 16:59:54 +0530

    project3

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const usercontroller= require("../controllers/UserController")
 const bookcontroller= require("../controllers/BookController")
+const reviewcontroller= require("../controllers/ReviewController")
 const ProjectMiddleware= require("../Middleware/middleware")
 
 router.post("/register",usercontroller.user)
@@ -12,6 +13,8 @@ router.get("/getbook",bookcontroller.booklist)
 router.get("/books/:bookId",bookcontroller.getBookReview)
 router.put("/books/:bookId",bookcontroller.updatebook)
 router.delete("/books/:bookId",bookcontroller.deleteBook)
+
+router.post("/createReview/:bookId",reviewcontroller.createReview)
 
 
 module.exports = router;
